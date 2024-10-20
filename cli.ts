31f94cdab86e05dbfd33250a3856da96ae234130
Leon Zhao <leeeon233@gmail.com>
2024-10-20 11:17:52 +0800

    feat: migrate to v1.0 (#1)
    * feat: migrate to v1.0
    
    * doc: readme

--- cli.ts
+++ cli.ts
@@ -2,18 +2,24 @@
 import yargs from "yargs/yargs";
 import { hideBin } from "yargs/helpers";
 import fs from "fs";
-import { migrate } from ".";
+import { LoroVersion, migrate } from ".";
 
 const args = yargs(hideBin(process.argv))
   .usage("Usage: loro-migrate [OPTIONS...]")
   .option("input", {
     alias: "i",
-    describe: "The path of snapshot exported from before v0.16",
+    describe: "The path of snapshot exported from before v1.0",
     type: "string",
   })
   .option("output", {
     alias: "o",
-    describe: "The path of converted snapshot of v0.16",
+    describe: "The path of converted snapshot of v1.0",
+    type: "string",
+  })
+  .option("from", {
+    alias: "f",
+    describe:
+      "The loro version of the input snapshot, choose from `v0.15`, `v0.16`",
     type: "string",
   })
   .help()
@@ -25,6 +31,8 @@ const argv = args.argv;
 const inputPath = argv.input || argv._[0];
 // @ts-ignore
 const outputPath = argv.output || argv._[1];
+// @ts-ignore
+const fromVersion = argv.from || argv._[2];
 
 if (!inputPath || !outputPath) {
   console.error("Please provide input and output path\n");
@@ -32,7 +40,23 @@ if (!inputPath || !outputPath) {
   process.exit(1);
 }
 
+let from;
+switch (fromVersion) {
+  case "v0.15":
+    from = LoroVersion.V015;
+    break;
+  case "v0.16":
+    from = LoroVersion.V016;
+    break;
+  default:
+    console.error(
+      "The loro version of the input snapshot, should be `v0.15` or `v0.16`"
+    );
+    args.showHelp();
+    process.exit(1);
+}
+
 const data = fs.readFileSync(inputPath);
-const snapshot = migrate(data);
+const snapshot = migrate(data, from);
 fs.writeFileSync(outputPath, snapshot);
 console.log("🦜 Snapshot converted successfully");
